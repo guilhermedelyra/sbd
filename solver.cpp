d8d0653aa83cbd1ca977cd9de21938906a73efce
Guilherme de Lyra <guilyra12@gmail.com>
2017-12-05 12:11:08 -0200

    100%?

--- solver.cpp
+++ solver.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <dirent.h>
 #define ll long long
 #define debug(x) cout << #x << " " << x << endl
 #define F first
@@ -10,68 +11,112 @@ using namespace std;
 
 int main ( ) {
 	int z;
-	cout << "insira o numero do grupo: "; cin >> z;
-	ifstream filein(to_string(z));
-    string read, read2, read3, op[4], in1, errmultes[6];
-    char buf[3];
-    while(getline(filein, read)){
-    	if (read == "ENUNCIADO") {
-    		getline(filein, read2);
-    		cout << read2 << "\n\n";
-    	}
-    	if (read == "TIPO DE QUESTAO") {
-    		getline(filein, read2);
-    		cout << "TIPO DE QUESTÃO = " << read2 << "\n\n";
-    		if (read2 == "VouF") {
-    			cout << "\n\nResposta (Verdadeiro ou Falso): "; 
-    			cin >> in1;
-    			getline (filein, read3);
+    do {
+        cout << "insira o numero do grupo (0 para sair)"; 
+        cout << "\n[lista de arquivos disponíveis: ";
 
-    			if (read3 == in1) {
-    				cout << "Parabéns!\n";
-    				printf("\n\nPressione qualquer tecla + <ENTER> para continuar...\n\n");
-    				scanf("%s", buf);
-    				system ("clear");
-    			} else {
-    				for (int i = 0; i < 2; ++i) getline (filein, read3);
-    				cout << read3 << endl;
-    				getline (filein, read);
-    				printf("\n\nPressione qualquer tecla + <ENTER> para continuar...\n\n");
-    				scanf("%s", buf);
-    				system ("clear");
-    			}
-    		}
+        
+        DIR *dirp;
+        struct dirent *directory;
 
-    		if (read2 == "Multipla-Escolha") {
-    			for (int i = 0; i < 4; ++i) {
-    				getline(filein, op[i]); cout << op[i] << endl;
-    			}    			
+        dirp = opendir("questoes/");
+        if (dirp) {
+            while ((directory = readdir(dirp)) != NULL) printf("%s ", directory->d_name);
+            closedir(dirp);
+        }
 
-    			cout << "\n\nResposta (apenas uma alternativa!): "; cin >> in1;
-    			getline (filein, read3);
+        cout << "]\n";
 
-    			if (read3 == in1) {
-    				cout << "Parabéns!\n";
-    				printf("\n\nPressione qualquer tecla + <ENTER> para continuar...\n\n");
-    				scanf("%s", buf);
-    				system ("clear");
-    			} else {
-    				for (int i = 0; i < 6; ++i) {
-    					getline(filein, errmultes[i]); cout << errmultes[i] << endl;
-    				}
-    				printf("\n\nPressione qualquer tecla + <ENTER> para continuar...\n\n");
-    				scanf("%s", buf);
-    				system ("clear");
-    			}
-    		}
+        scanf ("%d", &z); if (!z) break;
 
-    		if (read2 == "Escolha-Multipla") {
+    	ifstream filein(to_string(z));
+        string read, read2, read3, read4, op[4], in1, errmultes[6], in3;
+        char buf[3], in2[6];
 
-    		}
+        while(getline(filein, read)){
 
-    		if (read2 == "Lacunas") {
+        	if (read == "ENUNCIADO") {
+        		getline(filein, read2);
+        		cout << read2 << "\n\n";
+        	}
 
-    		}
-    	}
-    }
+        	if (read == "TIPO DE QUESTAO") {
+        		getline(filein, read2);
+        		cout << "TIPO DE QUESTÃO = " << read2 << "\n\n";
+
+        		if (read2 == "VouF") {
+        			cout << "\n\nResposta (Verdadeiro ou Falso): "; 
+        			cin >> in1;
+        			getline (filein, read3);
+
+        			if (tolower(read3[0]) == tolower(in1[0])) {
+        				cout << "Parabéns!\n";
+        			} else {
+        				cout << "\n\n";
+        				getline (filein, read3);
+        				cout << read3 << endl;
+        				cout << '\n';
+        			}
+        		}
+
+        		if (read2 == "Multipla-Escolha") {
+        			for (int i = 0; i < 4; ++i) {
+        				getline(filein, op[i]); cout << op[i] << endl;
+        			}    			
+
+        			cout << "\n\nResposta (apenas uma alternativa!): "; cin >> in1;
+        			getline (filein, read3);
+
+        			if (read3 == in1) {
+        				cout << "Parabéns!\n";
+        			} else {
+        				cout << "\n\n";
+        				for (int i = 0; i < 6; ++i) {
+        					getline(filein, errmultes[i]); cout << errmultes[i] << endl;
+        				}
+        				cout << "\n\n";
+        			}
+        		}
+
+        		if (read2 == "Escolha-Multipla") {
+        			for (int i = 0; i < 4; ++i) {
+        				getline(filein, op[i]); cout << op[i] << endl;
+        			}    			
+
+        			cout << "\n\nResposta (uma ou multiplas alternativas!): \n"; 
+        			
+        			scanf (" %[^\n]", in2);
+        			in3 = in2;
+        			getline (filein, read3);
+        			if (in3 == read3) {
+        				cout << "Parabéns!\n";
+        			} else {
+        				getline (filein, read4);
+        				cout << "\n\n" << read4 << "\n\n";
+        			}
+        		}
+
+        		if (read2 == "Lacunas") {
+        			getline (filein, read3); cout << read3;
+        			cout << "\n\nResposta: "; cin >> in1;
+        			getline (filein, read3);
+        			if (read3 == in1) {
+        				cout << "Parabens!\n";
+        			} else {
+        				getline (filein, read4);
+        				cout << "\n\n" << read4 << "\n\n";
+        			}
+        		}
+
+        		printf("\n\nPressione qualquer tecla + <ENTER> para continuar...\n\n");
+        		scanf("%s", buf);
+        		system ("clear");
+        	}
+        }
+
+        system ("clear");
+
+    } while (true);
+
+    return 0;
 }
